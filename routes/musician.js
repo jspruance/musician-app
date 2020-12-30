d924ac027f721c94fd9c7f2966adf0a0d227ffd2
Sandun Akalanka <30996264+sandunAkalanka@users.noreply.github.com>
2020-12-30 12:48:26 +0530

    Update musician.js
    Commented a major line to check if CI CD works

--- routes/musician.js
+++ routes/musician.js
@@ -1,4 +1,4 @@
-const express = require('express');
+//const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
