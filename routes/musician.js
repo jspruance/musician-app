217ae0b87d19dd743216552e7bd645354188e402
Farden <32020341+Farden@users.noreply.github.com>
2021-04-22 12:39:46 -0400

    Revert "Revert "ORE Test1"" (#6)

--- routes/musician.js
+++ routes/musician.js
@@ -50,7 +50,27 @@ router.put('/:id', jsonParser, async (req, res) => {
     res.status('400').send({errorMessage: 'Invalid request body'});
   }
 });
+// retrieve all musicians from data store
+router.get('/all', (req, res) => {
+  const { musician } = req.app.locals;
+  musician.getMusicians(req.params.id, (err, returnedMusicians) => {
+    if (err) {
+      res.status('400').send({errorMessage: err});
+    }
+    res.status('200').send(returnedMusicians);
+  });
+});
 
+// retrieve a musician from data store
+router.get('/:id', (req, res) => {
+  const { musician } = req.app.locals;
+  musician.getMusician(req.params.id, (err, returnedMusician) => {
+    if (err) {
+      res.status('400').send({errorMessage: err});
+    }
+    res.status('200').send(returnedMusician);
+  });
+});
 // retrieve a musician from data store
 router.delete('/:id', (req, res) => {
   const { musician } = req.app.locals;
